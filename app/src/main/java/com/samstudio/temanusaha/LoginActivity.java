522626dbb67ccf90be3ebd8343aa930da9822407
Satria Agung M <satryaway@gmail.com>
2015-09-10 21:29:56 +0700

    [Design] Login Layout

---
+++ app/src/main/java/com/samstudio/temanusaha/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.samstudio.temanusaha;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by satryaway on 9/10/2015.
+ * Login Activity
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
